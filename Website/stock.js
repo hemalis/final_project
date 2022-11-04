d6167bd7ab4cbc2501625acbc90814a0e9e6d877
Ziying Pan <taiqinwu1999@gmail.com>
2022-11-04 01:07:18 -0700

    making website

--- Website/stock.js
+++ Website/stock.js
@@ -1,38 +1,53 @@
-d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv', function(err, rows){
+d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv", function(err, rows){
 
-function unpack(rows, key) {
-  return rows.map(function(row) {
-    return row[key];
-  });
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
 }
 
-var trace = {
-  x: unpack(rows, 'Date'),
-  close: unpack(rows, 'AAPL.Close'),
-  high: unpack(rows, 'AAPL.High'),
-  low: unpack(rows, 'AAPL.Low'),
-  open: unpack(rows, 'AAPL.Open'),
-
-  // cutomise colors
-  increasing: {line: {color: 'black'}},
-  decreasing: {line: {color: 'red'}},
-
-  type: 'candlestick',
-  xaxis: 'x',
-  yaxis: 'y'
+var trace1 = {
+  type: "scatter",
+  mode: "lines",
+  name: "AAPL Actual",
+  x: unpack(rows, "Date"),
+  y: unpack(rows, "AAPL.High"),
+  line: {color: "#ea335d"},
 };
 
-var data = [trace];
+var trace2 = {
+  type: "scatter",
+  mode: "lines",
+  name: "AAPL Predict",
+  x: unpack(rows, "Date"),
+  y: unpack(rows, "AAPL.Low"),
+  line: {color: "#03dcee"},
+};
+
+var data = [trace1,trace2];
 
 var layout = {
-  dragmode: 'zoom',
-  showlegend: false,
+  paper_bgcolor: "#172042",
+  plot_bgcolor: "#172042",
+  showlegend: true,
+  margin: {
+    l: 30,
+    r: 30,
+    b: 30,
+    t: 30,
+    pad: 1,
+  },
+  font: {
+    color: "#6b5f8a"
+  },
+  title: 'Prediction Graph',
   xaxis: {
-    rangeslider: {
-		 visible: false
-	 }
+    range: ['2015-02-17', '2017-02-16'],
+    type: 'date'
+  },
+  yaxis: {
+    autorange: true,
+    type: 'linear'
   }
 };
 
 Plotly.newPlot('AAPL', data, layout);
-});
+})
